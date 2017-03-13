83f5f7ea3ec3fbff15faa5966182ab2ba73d1e1a
DominicanCoder <rflores.rsj@gmail.com>
2017-03-12 20:34:14 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,25 @@
 console.log("Up and running!");
+
 var cardOne = "queen";
+cardOne = "queen";
+console.log = ("User flipped" + cardOne);
+
 var cardTwo = "queen";
+cardTwo = "queen";
+console.log = ("User flipped" + cardTwo);
+
 var cardThree = "king";
-var cardTwo = "king";
-queen = "cardOne";
-queen = "cardTwo";
-king = "cardThree";
-king = "cardFour";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardThree = "king";
+console.log = ("User flipped" + cardThree);
+
+var cardFour = "king";
+cardFour = "king";
+console.log = ("User flipped" + cardFour);
+
+
+
+
+
+
+
+
