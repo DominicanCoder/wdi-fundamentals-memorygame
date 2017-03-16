b42c4a8c67e62736b293cf2521b560511bcc0945
DominicanCoder <rflores.rsj@gmail.com>
2017-03-15 23:28:30 -0600

    create alert-Hello-Friends

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,24 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-cardOne = "queen";
-console.log = ("User flipped" + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = ["cardOne"];
+var cardOne =  ["queen"];
+cardOne = ["0"];
+cardsInPlay .push("cardOne");
+console.log("User flipped queen");
 
-var cardTwo = "queen";
-cardTwo = "queen";
-console.log = ("User flipped" + cardTwo);
+var cardTwo =  ["queen"];
+cardOne = ["1"];
+cardsInPlay.push("cardTwo");
+console.log("User flipped queen");
 
-var cardThree = "king";
-cardThree = "king";
-console.log = ("User flipped" + cardThree);
+var cardThree =  ["king"];
+cardOne = ["2"];
+cardsInPlay.push("cardThree");
+console.log("User flipped king");
 
-var cardFour = "king";
-cardFour = "king";
-console.log = ("User flipped" + cardFour);
-
-
-
-
-
-
-
-
+var cardFour =  ["king"];
+cardOne = ["3"];
+cardsInPlay.push("cardFour");
+console.log("User flipped king");
+alert('Hello, friends.');
